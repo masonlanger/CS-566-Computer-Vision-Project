1f13b5d74712a0c3a6b20e4502dc517bb3929fd4
William Cong <wcong@Williams-MacBook-Pro-6.local>
2025-10-17 15:40:49 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ gdown -O "$DIR/models/football-player-detection.pt" "https://drive.google.com/uc
 gdown -O "$DIR/models/football-pitch-detection.pt" "https://drive.google.com/uc?id=1Ma5Kt86tgpdjCTKfum79YMgNnSjcoOyf"
 
 # download the videos
-gdown -O "$DIR/models/video_1.mp4" "https://drive.google.com/uc?id=12TqauVZ9tLAv8kWxTTBFWtgt2hNQ4_ZF"
-gdown -O "$DIR/models/video_2.mp4" "https://drive.google.com/uc?id=19PGw55V8aA6GZu5-Aac5_9mCy3fNxmEf"
-gdown -O "$DIR/models/video_3.mp4" "https://drive.google.com/uc?id=1OG8K6wqUw9t7lp9ms1M48DxRhwTYciK-"
-gdown -O "$DIR/models/video_4.mp4" "https://drive.google.com/uc?id=1yYPKuXbHsCxqjA9G-S6aeR2Kcnos8RPU"
-gdown -O "$DIR/models/video_5.mp4" "https://drive.google.com/uc?id=1vVwjW1dE1drIdd4ZSILfbCGPD4weoNiu"
+gdown -O "$DIR/videos/video_1.mp4" "https://drive.google.com/uc?id=12TqauVZ9tLAv8kWxTTBFWtgt2hNQ4_ZF"
+gdown -O "$DIR/videos/video_2.mp4" "https://drive.google.com/uc?id=19PGw55V8aA6GZu5-Aac5_9mCy3fNxmEf"
+gdown -O "$DIR/videos/video_3.mp4" "https://drive.google.com/uc?id=1OG8K6wqUw9t7lp9ms1M48DxRhwTYciK-"
+gdown -O "$DIR/videos/video_4.mp4" "https://drive.google.com/uc?id=1yYPKuXbHsCxqjA9G-S6aeR2Kcnos8RPU"
+gdown -O "$DIR/videos/video_5.mp4" "https://drive.google.com/uc?id=1vVwjW1dE1drIdd4ZSILfbCGPD4weoNiu"
