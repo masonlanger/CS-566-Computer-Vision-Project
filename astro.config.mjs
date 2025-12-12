864c20031d8bef54a720c6c86378fa1abcce619b
masonlanger <masonlanger00@gmail.com>
2025-12-11 19:31:06 -0600

    Cleaning up repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://masonlanger.github.io',
 	base: '/CS-566-Computer-Vision-Project',
+	markdown: {
+		syntaxHighlight: "prism",
+		remarkPlugins: [remarkMath],
+    	rehypePlugins: [rehypeKatex],
+	}
 });
